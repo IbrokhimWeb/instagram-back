400db915053843a7b6aeb6418f43067000a03f13
IbrokhimWeb <ibrokhimweb@gmail.com>
2022-12-26 18:04:19 +0500

    mongos url edited

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 exports.connectToDB = async () => {
-    mongoose.connect("mongodb+srv://ibo:1234@azaza.endngxs.mongodb.net/?retryWrites=true&w=majority").then((connection)=>{
+    mongoose.connect("mongodb+srv://IbrokhimWeb:Ibroxim7845@instagram-backend.r2dgybc.mongodb.net/?retryWrites=true&w=majority").then((connection)=>{
         console.log("DB connected : "+ connection.connection.host);
     }).catch(err=>{
         console.log(err);
